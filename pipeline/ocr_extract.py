d799b0ebb3acf6b8046153d607855620b0f1e7c3
AaravK25 <aaravkoul25@gmail.com>
2026-03-08 12:13:24 +0530

    ocr extarction code almost complete

--- pipeline/ocr_extract.py
+++ pipeline/ocr_extract.py
@@ -1,1 +1,20 @@
-#PaddleOCR extraction
+from paddleocr import PaddleOCR
+
+ocr = PaddleOCR(
+    use_doc_orientation_classify=False,
+    use_doc_unwarping=False,
+    use_textline_orientation=False,
+    text_detection_model_name="PP-OCRv5_mobile_det",
+    text_recognition_model_name="PP-OCRv5_mobile_rec",
+    device='gpu:0'
+)
+
+result = ocr.predict(input="data/processed_images/IMG_20260205_175116.jpg")
+
+for res in result:
+    texts = res['rec_texts']
+    scores = res['rec_scores']
+    boxes = res['rec_boxes']
+
+    for text, score, box in zip(texts, scores, boxes):
+        print(f"[{score:.3f}] {text}")
