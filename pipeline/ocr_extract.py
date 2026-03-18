8dfac1351eeef77b09e5baba4f7b2c6e6a7954d0
AaravK25 <aaravkoul25@gmail.com>
2026-03-18 20:01:18 +0530

    Added a Text cleaner

--- pipeline/ocr_extract.py
+++ pipeline/ocr_extract.py
@@ -7,8 +7,8 @@ ocr = PaddleOCR(
     use_doc_orientation_classify=False,
     use_doc_unwarping=False,
     use_textline_orientation=False,
-    text_detection_model_name="PP-OCRv5_mobile_det",
-    text_recognition_model_name="PP-OCRv5_mobile_rec",
+    text_detection_model_name="PP-OCRv5_server_det",
+    text_recognition_model_name="PP-OCRv5_server_rec",
     device='gpu:0'
 )
 
--- pipeline/text_clean.py
+++
@@ -1,1 +0,0 @@
-#Cleaning OCR text
